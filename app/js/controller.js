7c7147ad6c5a702cca8c4eb49022b0499faaeff0
cbalit <dou4872>
2015-01-26 17:05:07 +0100

    add impl

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,68 +1,115 @@
-if(handson == undefined){
+if (handson == undefined) {
     var handson = {};
 }
 
 
 handson.Controller = Class.extend({
+    BY_NAME: "filter-name",
+    BY_ROLES: "filter-roles",
+    datas: [],
+    filterParams: {},
+    filters: [],
+    table: null,
 
-    datas:[],
-    filters:[],
-    table:null,
-
-    init : function(table) {
-        this.table= table;
+    init: function (table) {
+        this.table = table;
+        $('#chk-roles').hide();
+        this.filterParams = {
+            by: this.BY_NAME,
+            value: '',
+            roles: []
+        };
         this.addEvent();
     },
 
-    setDatas : function(datas){
-        this.datas= datas;
+    setDatas: function (datas) {
+        this.datas = datas;
     },
 
-    setFilter : function(filter){
-        this.filter= filter;
+    setFilter: function (filter) {
+        this.filter = filter;
     },
 
-    addEvent : function(){
-        var _this=this;
+    addEvent: function () {
+        var _this = this;
+        $('#filter-input').keyup(function (e) {
+            _this.inputChangeHandler();
+        });
         $('#filter-button').click(function (e) {
             _this.clickHandler();
+        });
+        $('#optionsRadios1').click(function (e) {
+            _this.filterBy(_this.BY_NAME);
+            $('#chk-roles').hide();
+        });
+        $('#optionsRadios2').click(function (e) {
+            _this.filterBy(_this.BY_ROLES);
+            $('#chk-roles').show();
+        });
+        $('.checkbox input').click(function (e) {
+            _this.toggleRoles(e.target.value);
         })
     },
-
-    clickHandler : function(e){
-        var value=$('#filter-input').val();
-        this.filterDatas(value);
+    inputChangeHandler: function () {
+        this.filterParams.value = $('#filter-input').val();
+        this.filterDatas();
+    },
+    clickHandler: function (e) {
+        var value = $('#filter-input').val();
+        this.filterDatas();
+        //this.filterDatas(value);
     },
 
-    filterDatas : function(value){
-        if(value){
-            var filterDatas=this.filter.filterByName(this.datas,value);
+    filterBy: function (filterName) {
+        this.filterParams.by = filterName;
+        this.filterDatas();
+    },
+    toggleRoles: function (role) {
+        var ind = this.filterParams.roles.indexOf(role);
+        if (ind == -1) {
+            this.filterParams.roles.push(role);
+        }
+        else {
+            this.filterParams.roles.splice(ind, 1);
+        }
+        this.filterDatas();
+    },
+    filterDatas: function (value) {
+        console.log('FILTER......', this.filterParams);
+        var filterDatas;
+        //if (this.filterParams.value) {
+            if (this.filterParams.by == this.BY_NAME) {
+                filterDatas = this.filter.filterByName(this.datas, this.filterParams.value);
+            }
+            else if (this.filterParams.by == this.BY_ROLES) {
+                filterDatas = this.filter.filterByRoles(this.datas, this.filterParams.roles);
+            }
             this.updateView(filterDatas);
-        }
-        else{
+        /*}
+        else {
             this.resetView();
-        }
+        }*/
 
     },
 
-    updateView : function(datas){
+    updateView: function (datas) {
         var datas = datas || this.datas;
-        var body=this.table.find('tbody');
+        var body = this.table.find('tbody');
         body.empty();
-        if(datas && datas instanceof Array) {
-            datas.forEach(function(item){
+        if (datas && datas instanceof Array) {
+            datas.forEach(function (item) {
                 body.append('<tr>' +
-                '<td>'+item.id+'</td>' +
-                '<td>'+item.firstname+' '+item.lastname+'</td>' +
-                '<td>'+item.mail+'</td>' +
-                '<td>'+item.company+'</td>' +
-                '<td>'+item.roles+'</td>' +
+                '<td>' + item.id + '</td>' +
+                '<td>' + item.firstname + ' ' + item.lastname + '</td>' +
+                '<td>' + item.mail + '</td>' +
+                '<td>' + item.company + '</td>' +
+                '<td>' + item.roles + '</td>' +
                 '</tr>')
             });
         }
     },
 
-    resetView : function(){
+    resetView: function () {
         this.updateView(this.datas);
     }
 });
--- app/js/filter.js
+++ app/js/filter.js
@@ -1,23 +1,43 @@
 /**
  * Created by adzs637 on 19/01/2015.
  */
-if(handson == undefined){
+if (handson == undefined) {
     var handson = {};
 }
 
 handson.Filter = Class.extend({
     //Filter on a array of object
     //check that the object have the key
-    filterByName : function(datas,value) {
-        var filterDatas=[];
-        if(datas && datas instanceof Array && datas.length>0) {
+    filterByName: function (datas, value) {
+        var filterDatas = [];
+        if (datas && datas instanceof Array && datas.length > 0) {
             if (value) {
                 filterDatas = datas.filter(function (item) {
-                    return (item.lastname.toLowerCase().indexOf(value.toLowerCase()) != -1 || item.firstname.toLowerCase().indexOf(value.toLowerCase())!= -1);
+                    return (item.lastname.toLowerCase().indexOf(value.toLowerCase()) != -1 || item.firstname.toLowerCase().indexOf(value.toLowerCase()) != -1);
                 });
             }
-            else{
-                filterDatas=datas
+            else {
+                filterDatas = datas
+            }
+        }
+        return filterDatas;
+    },
+    filterByRoles: function (datas, roles) {
+        var filterDatas = [];
+        if (datas && datas instanceof Array && datas.length > 0) {
+            if (roles && roles instanceof Array && roles.length > 0) {
+                $(datas).each(function(index,data) {
+                    var hasAllRole = true;
+                    $(roles).each(function (index, role) {
+                        hasAllRole = hasAllRole && ($.inArray(role, data.roles) != -1);
+                    });
+                    if (hasAllRole) {
+                        filterDatas.push(data);
+                    }
+                });
+            }
+            else {
+                filterDatas = datas
             }
         }
         return filterDatas;
